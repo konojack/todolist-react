82bd0d69e524e4c5bc9bab2c5ac21862da45cbbd
Dawid Konojacki <konojack@gmail.com>
2018-12-13 20:30:57 +0100

    Edit form with Formik

--- src/components/ToDoEditForm/index.js
+++ src/components/ToDoEditForm/index.js
@@ -1,11 +1,23 @@
-import React, { Component } from 'react';
-import { get, update } from '../../helpers/toDoItemApi';
-import { Formik } from 'formik';
+import React, { Component } from 'react'
+import { get, update } from '../../helpers/toDoItemApi'
+import { Formik } from 'formik'
+import { withRouter } from 'react-router-dom'
+import {
+    SubmitButton,
+    TextInput,
+    Label,
+    Select,
+    ErrorMsg
+  } from '../../helpers/theme'
+import * as _ from 'ramda'
+import { runInThisContext } from 'vm';
+
 
 class ToDoEditForm extends Component {
     state = {
         toDoItem: null,
-        fetched: false
+        fetched: false,
+        disabled: false
     }
 
     componentDidMount = async () => {
@@ -22,8 +34,27 @@ class ToDoEditForm extends Component {
                 {this.state.fetched
                     ? <Formik 
                         initialValues={{...this.state.toDoItem}}
-                        onSubmit={(values) => {
-                            update(this.itemId(), {...values})
+                        onSubmit={async (values) => {
+                            await update(this.itemId(), {...values})
+                            this.props.history.push('/')
+                        }}
+                        validate={(values) => {
+                            let errors = {}
+                            if(!values.content) {
+                                errors.content = "Required"
+                            } else if (values.content.length < 3) {
+                                errors.content = 'Too short. Minimum 3 chars!'
+                            } else if (values.content.includes('ass')) {
+                                errors.content = 'Mind your language!'
+                            }
+
+                            if(_.isEmpty(errors)) {
+                                this.setState({disabled: false})
+                            } else {
+                                this.setState({disabled: true})
+                            }
+
+                            return errors;
                         }}
                         render={({
                             values,
@@ -35,13 +66,40 @@ class ToDoEditForm extends Component {
                             isSubmitting
                         }) => (
                             <form onSubmit={handleSubmit}>
-                                <input 
-                                    name="content" 
-                                    onChange={handleChange}
-                                    value={values.content}
-                                />
+                                <Label>
+                                    Content *
+                                    <ErrorMsg>{errors.content}</ErrorMsg>
+                                    <TextInput 
+                                        name="content" 
+                                        onChange={handleChange}
+                                        value={values.content}
+                                    />
+                                </Label>
+
+                                <Label>
+                                    Priority
+                                    <Select 
+                                        name="priority" 
+                                        onChange={handleChange}
+                                        value={values.priority}>
+                                        <option value="low">Low</option>
+                                        <option value="high">High</option>
+                                        <option value="urgent">Urgent</option>
+                                    </Select>
+                                </Label>
+
+                                <Label>
+                                    Done ?
+                                    <input 
+                                        type="checkbox" 
+                                        name="done"
+                                        value={values.done}
+                                        checked={values.done}
+                                        onChange={handleChange}
+                                    />
+                                </Label>
                                 <br/>
-                                <button type="submit">Update</button>
+                                <SubmitButton type="submit" disabled={this.state.disabled}>Update</SubmitButton>
                             </form>
                         )}
                      />
@@ -52,4 +110,4 @@ class ToDoEditForm extends Component {
     }
 }
 
-export default ToDoEditForm
+export default withRouter(ToDoEditForm)
